2da681ad1559db4e8a61824d8361236c9de01d68
sprutalekc <sprutalekc@gmail.com>
2023-10-06 11:23:30 +0800

    first

---
+++ test/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="square"></div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+.square{
+  height: 40px;
+  width: 40px;
+  background-color: red;
+}
+</style>
